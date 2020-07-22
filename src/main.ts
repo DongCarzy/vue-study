23454584fda3f6ab96ddf2286c3096a9350abbdc
carzy <carzy.dong@tonmx.com>
2020-07-22 16:27:27 +0800

    fix(matcher): 升级vue-route
    解决navigate to same as current location

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
 
+// 开发环境下，Vue 会提供很多警告来帮你对付常见的错误与陷阱
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
